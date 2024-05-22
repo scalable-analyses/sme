333b5505f5ecdac760a872db2d2bd798f09f5845
Alexander Nikolas Breuer <breuer@mytum.de>
2024-05-22 22:44:34 +0200

    Fixed indentation.

--- kernels_gemm.h
+++ kernels_gemm.h
@@ -1,5 +1,5 @@
 extern "C" {
-    void gemm_micro_64_16_2( float const * i_a,
-                             float const * i_b,
-                             float       * io_c );
+  void gemm_micro_64_16_2( float const * i_a,
+                           float const * i_b,
+                           float       * io_c );
 }
